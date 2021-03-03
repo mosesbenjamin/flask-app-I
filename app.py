c20756ca17bd265771df7c6686add4d9c70cf397
mosesbenjamin <mavewreek@gmail.com>
2021-03-02 21:49:14 -0600

    Routing and serving dynamic content with route parameters

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# @app.route('/greet')
+# def greet():
+#     name = "Mavewrick"
+#     return render_template('greet.html', name = name)
+
+@app.route('/<name>')
+def nameParam(name):
+    name = name
+    return render_template('greet.html', name = name)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
